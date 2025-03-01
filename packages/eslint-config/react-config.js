daa8ba978cf6b6e7de85a3695dc8bdc74a95a023
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2025-03-01 07:35:32 +0100

    Update config for eslint-plugin-react-hooks

--- packages/eslint-config/react-config.js
+++ packages/eslint-config/react-config.js
@@ -2,11 +2,11 @@ import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 
 export const reactConfig = [
+    ...reactHooks.configs['recommended-latest'],
     {
         files: ['**/*.jsx', '**/*.tsx'],
         plugins: {
             react,
-            reactHooks,
         },
         settings: {
             react: {
